fe27078b7b6da9787003d3be20a9bd4bcb4bbd86
Cyril <gavrosh.cyril@gmail.com>
2023-04-13 18:28:11 +0300

    feat: create new component for my project

---
+++ src/ImageGallery/images.js
@@ -0,0 +1,88 @@
+
+export const mosaicImages = [
+    {
+      src: "http://content.onliner.by/news/2014/06/default/e2c7116177f6745ca2d2b41f2e62c6dc.jpg",
+      alt: "В конце 1960-х годов Минск столкнулся с типичной проблемой многих крупных городов.",
+      caption: "README.md",
+      width: 2000,
+      height: 1469
+    },
+    {
+      src:
+        "http://content.onliner.by/news/2014/06/default/210de5314462e21e61de32a9a0ea0b24.jpg",
+      alt: "Windows 10 Dark Mode Setting",
+      caption: "Windows 10 Dark Mode Setting",
+      width: 2848,
+      height: 2035
+    },
+    {
+      src:
+        "http://content.onliner.by/news/2014/06/default/06478c564fc0a660df66dbded9cb1cf7.jpg",
+      alt: "macOS Mojave Dark Mode Setting",
+      caption: "macOS Mojave Dark Mode Setting",
+      width: 1200,
+      height: 1218
+    },
+    {
+      src:
+        "http://content.onliner.by/news/2014/06/default/78fe9472256e2d1beb0367761b3cd833.jpg",
+      alt: "Android 9.0 Dark Mode Setting",
+      caption: "Android 9.0 Dark Mode Setting",
+      width: 1280,
+      height: 600
+    },
+    {
+      src: "http://content.onliner.by/news/2014/06/default/8c2652652459d8ba6e34c6b7082da5b8.jpg",
+      alt: "README.md",
+      caption: "README.md",
+      width: 2486,
+      height: 1469
+    },
+    {
+      src:
+        "http://content.onliner.by/news/2014/06/default/6ab55b56a9079272aaa99018bf7d35ad.jpg",
+      alt: "Windows 10 Dark Mode Setting",
+      caption: "Windows 10 Dark Mode Setting",
+      width: 2848,
+      height: 2035
+    },
+    {
+      src:
+        "http://content.onliner.by/news/2014/06/default/934fbfe2ddfe7bebc46f5adbafb24b28.jpg",
+      alt: "macOS Mojave Dark Mode Setting",
+      caption: "macOS Mojave Dark Mode Setting",
+      width: 1200,
+      height: 1218
+    },
+    {
+      src:
+        "http://content.onliner.by/news/2014/06/default/be9d9cb3d4b714f4b5322c84643f3b16.jpg",
+      alt: "Android 9.0 Dark Mode Setting",
+      width: 1280,
+      height: 600
+    },
+    {
+        src:
+          "http://content.onliner.by/news/2014/06/default/bf66fd25da1745457bb72a67cbce72fe.jpg",
+        alt: "Android 9.0 Dark Mode Setting",
+        caption: "Android 9.0 Dark Mode Setting",
+        width: 1280,
+        height: 600
+      }
+      ,
+    {
+        src:
+          "http://content.onliner.by/news/2014/06/default/8591219b5b8ff9b6d7d01af25c3beafc.jpg",
+        alt: "Android 9.0 Dark Mode Setting",
+        caption: "Android 9.0 Dark Mode Setting",
+        width: 1280,
+        height: 600
+      }
+  ];
+  
+  export const lightboxImages = mosaicImages.map(({ src, alt, caption }) => ({
+    src,
+    alt,
+    caption
+  }));
+  
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
+import { AppContainer, GlobalStyles } from "./styles";
+import ImageGallery from "./ImageGallery";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const App = () => (
+  <ThemeProvider
+    theme={{
+      pageBackgroundColor: "#101010",
+      pageContentFontColor: "#e2e5ec",
+      pageContentLinkHoverColor: "aquamarine",
+      pageContentSelectionColor: "aquamarine",
+      headerNavFontColor: "#e2e5ec",
+      accentColor: "#1f1f1f"
+    }}
+  >
+    <AppContainer>
+      <GlobalStyles />
+      <ImageGallery />
+    </AppContainer>
+  </ThemeProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
